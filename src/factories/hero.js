61a8bbec86738502de525d847a9b67d5f375cb1f
Kelvin de Miranda Barros <kmbcg@hotmail.com>
2022-06-16 14:14:07 -0300

    Finished hero factory.

--- src/factories/hero.js
+++ src/factories/hero.js
@@ -1,7 +1,14 @@
 import HeroRepository from '../repositories/hero';
 import HeroService from '../services/hero';
 
-const generateInstance = ({ filePath }) => {
+const generate_instance = ({ file_path }) => {
   // Here goes all db connections.
-  const hero_repository = new HeroRepo();
+  const hero_repository = new HeroRepository({ file: file_path });
+  const hero_service = new HeroService({
+    hero_repository
+  });
+
+  return hero_service;
 };
+
+export { generate_instance };
